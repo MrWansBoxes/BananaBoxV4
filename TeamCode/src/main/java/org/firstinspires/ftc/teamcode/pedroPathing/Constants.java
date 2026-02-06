5120ea4981f9d08da4d270f8d8d5865731614d97
Ethan Deandrea <119320897+Theo-928@users.noreply.github.com>
2026-02-06 18:46:12 -0500

    Finished tuning pedro (#9)

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/pedroPathing/Constants.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/pedroPathing/Constants.java
@@ -17,12 +17,12 @@ import org.firstinspires.ftc.robotcore.external.navigation.DistanceUnit;
 public class Constants {
     public static FollowerConstants followerConstants = new FollowerConstants()
             .mass(13)   // mass in kg
-           // .forwardZeroPowerAcceleration()   // have to be negative
-          //  .lateralZeroPowerAcceleration()
-          //  .translationalPIDFCoefficients(new PIDFCoefficients(0.1, 0, 0.01, 0.02))    // these are the PIDF
-          //  .headingPIDFCoefficients(new PIDFCoefficients(1, 0, 0.02, 0.01))
-          //  .drivePIDFCoefficients(new FilteredPIDFCoefficients(0.025,0.0,0.00001,0.6,0.01))
-          //  .centripetalScaling(0.0001);
+            .forwardZeroPowerAcceleration(-42.944916498283)   // have to be negative
+            .lateralZeroPowerAcceleration(-53.72242120209232)
+            .translationalPIDFCoefficients(new PIDFCoefficients(0.15, 0, 0.015, 0.04))    // these are the PIDF
+            .headingPIDFCoefficients(new PIDFCoefficients(1, 0, 0.04, 0.05))
+            .drivePIDFCoefficients(new FilteredPIDFCoefficients(0.05,0.0,0.00001,0.6,0.05))
+            .centripetalScaling(0.0005);
 ;
 
     public static MecanumConstants driveConstants = new MecanumConstants()
@@ -35,8 +35,8 @@ public class Constants {
             .leftRearMotorDirection(DcMotorSimple.Direction.REVERSE)
             .rightFrontMotorDirection(DcMotorSimple.Direction.FORWARD)
             .rightRearMotorDirection(DcMotorSimple.Direction.FORWARD)
-          //  .xVelocity()
-          //  .yVelocity();
+            .xVelocity(71.80810546874999)
+            .yVelocity(56.209573580524115)
     ;
     public static PinpointConstants localizerConstants = new PinpointConstants()
             .forwardPodY(-3)   // Y offset
@@ -45,9 +45,9 @@ public class Constants {
             .hardwareMapName("pinpoint")  // hardware map for pinpoint computer
             .encoderResolution(GoBildaPinpointDriver.GoBildaOdometryPods.goBILDA_4_BAR_POD)
             .forwardEncoderDirection(GoBildaPinpointDriver.EncoderDirection.FORWARD)   // Encoder direction
-            .strafeEncoderDirection(GoBildaPinpointDriver.EncoderDirection.FORWARD);
+            .strafeEncoderDirection(GoBildaPinpointDriver.EncoderDirection.REVERSED);
 
-    public static PathConstraints pathConstraints = new PathConstraints(0.99, 100, 1, 1);
+    public static PathConstraints pathConstraints = new PathConstraints(0.99, 100, 1, 0.3);
 
     public static Follower createFollower(HardwareMap hardwareMap) {
         return new FollowerBuilder(followerConstants, hardwareMap)
